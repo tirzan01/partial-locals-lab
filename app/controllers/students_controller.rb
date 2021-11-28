af09c4a1e5a113adc6e71fef3f23807f959cf04e
Thomas Montalto <thomasmonta98@gmail.com>
2021-11-28 19:01:02 +1100

    done with assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,10 +21,10 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    @students = Student.search(params[:query])
   end
 
   def student_params
-    params.require(:student).permit(:name, :birthday, :hometown)
+    params.require(:student).permit(:name, :birthday, :hometown, :query)
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    unless query || query.blank?
+      self.all
+    else
+      self.all.select{ |s| s.name.include?(query.titlecase)}
+    end
+  end
 end
